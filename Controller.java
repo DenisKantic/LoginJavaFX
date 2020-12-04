8d9464e84291428613958ca63292c4f1545305e6
DenisKantic <denis.kantic18@gmail.com>
2020-12-04 09:59:39 +0100

    added some changes in css for buttons, corrected labels for status user input and added controller option

--- Controller.java
+++ Controller.java
@@ -1,4 +1,39 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import static javafx.application.Platform.exit;
+
 public class Controller {
+
+    @FXML
+    private TextField usernameBox;
+
+    @FXML
+    private PasswordField passwordBox;
+
+    @FXML
+    private Label correctInput;
+
+    @FXML
+    private Label wrongPassword;
+
+
+    public void checkPassword(ActionEvent event) {
+        if (usernameBox.getText().equals("user@domain.com") && passwordBox.getText().equals("1234")) {
+            wrongPassword.setText("");
+            correctInput.setText("Login successful");
+        } else {
+            correctInput.setText("");
+            wrongPassword.setText("Username or password not correct");
+        }
+    }
+
+    public void close(ActionEvent event){
+        exit();
+    }
 }
